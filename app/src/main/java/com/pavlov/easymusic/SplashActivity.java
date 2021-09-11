2c3455562c9687d25f3cf31aaa886bccd2d81bbd
Pavlo <pavlovpavlo2013@gmail.com>
2021-09-11 16:41:27 +0300

    add load animation to splash activity

--- app/src/main/java/com/pavlov/easymusic/SplashActivity.java
+++ app/src/main/java/com/pavlov/easymusic/SplashActivity.java
@@ -1,14 +1,55 @@
 package com.pavlov.easymusic;
 
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView cardPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        initViews();
     }
+
+    private void initViews() {
+        cardPlay = findViewById(R.id.card_play);
+
+        startAnimation();
+    }
+
+    private void startAnimation() {
+        cardPlay.animate().withLayer()
+                .rotationX(720)
+                .setDuration(3000)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                }).start();
+    }
+
 }
